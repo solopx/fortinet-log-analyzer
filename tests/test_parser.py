e1662615054992d614887a471398b737f745defa
Rafael Cartarozzi Lucio <rcartarozzi@gmail.com>
2026-02-26 20:42:46 -0300

    V2

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -3,7 +3,7 @@ import os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from old_versions.main import parse_line
+from src.main import parse_line
 
 
 def test_parse_fortigate_log_line():
